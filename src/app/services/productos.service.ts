b06fc76ade3ea457248cb00a4e7d35797eee2cb5
jhoysi93 <jhoysi939@gmail.com>
2017-08-15 10:54:16 -0400

    Primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -7,31 +7,72 @@ export class ProductosService {
 
   productos: any[] = [];
   cargandoProductos: boolean = true;
+  productosFiltrados: any[] = [];
 
-  constructor( private http: Http) {
+  constructor(private http: Http) {
     this.cargarProductos();
   }
 
 
-  public cargarProductos(){
-    if( this.productos.length == 0 ){
+  public cargarProductos() {
+    if (this.productos.length == 0) {
 
       this.cargandoProductos = true;
 
-      this.http.get('https://paginaweb-22df1.firebaseio.com/productos_idx.json')
-                .subscribe( res =>{
-                 // console.log( res.json() );
+      let promesa = new Promise((resolve, reject) => {
+        this.http.get('https://paginaweb-22df1.firebaseio.com/productos_idx.json')
+          .subscribe(res => {
+            // console.log( res.json() );
 
-                 // setTimeout( ()=>{
-                    this.cargandoProductos = false;
-                    this.productos = res.json();
+            // setTimeout( ()=>{
+            this.cargandoProductos = false;
+            this.productos = res.json();
+            resolve();
+            //}, 1500)
+          });
 
-                  //}, 1500)
-                });
+      });
+      return promesa;
+
     }
   }
 
-  public cargarProducto( cod: string ){
-    return this.http.get(`https://paginaweb-22df1.firebaseio.com/productos/${ cod }.json`)
+  public cargarProducto(cod: string) {
+    return this.http.get(`https://paginaweb-22df1.firebaseio.com/productos/${cod}.json`)
+  }
+
+
+  public buscarProducto(termino: string) {
+
+    //console.log("Buscando Productos");
+    //console.log(this.productosFiltrados.length);
+
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() => {
+        //termino la carga
+        this.filtrarProductos( termino );
+      });
+    } else {
+        this.filtrarProductos( termino );
+    }
+
+
+  }
+
+
+  public filtrarProductos(termino: string) {
+
+    this.productosFiltrados = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+
+      if (prod.categoria.indexOf( termino ) >= 0 || prod.titulo.toLowerCase().indexOf( termino ) >= 0 ) {
+        this.productosFiltrados.push( prod );
+        //console.log( prod );
+      }
+
+      //console.log(prod);
+    });
   }
 }
